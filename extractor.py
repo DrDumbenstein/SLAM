93f741ab6ca215c63586fc539ba80afa548172bd
DrDumbenstein <seddik_yousfi@yahoo.com>
2020-06-22 11:14:33 +0100

    Normalized coords

--- extractor.py
+++ extractor.py
@@ -1,15 +1,28 @@
 import cv2
 import numpy as np
 from skimage.measure import ransac
-from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform, FundamentalMatrixTransform
+
+
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
 
 class Extractor(object):
 
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create(100)
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+
+    def normalize(self, pt):
+        return np.dot(self.Kinv, add_ones(pt).T).T[:, 0:2]
+
+    def denormalize(self, pt):
+        ret = np.array(np.dot(self.K, [pt[0], pt[1], 1.0]))
+        return int(round(ret[0])), int(round(ret[1]))
 
     def extract(self, img):
         # Detection
@@ -29,16 +42,26 @@ class Extractor(object):
                     kp2 = self.last['kps'][m.trainIdx].pt
                     ret.append((kp1, kp2))
 
+
+
         # Filter
         matches = []
         if len(ret) > 0:
             ret = np.array(ret)
+            # Normalize coors: substract to move to 0
+            ret[:, 0, :] = self.normalize(ret[:, 0, :])
+            ret[:, 1, :] = self.normalize(ret[:, 1, :])
+
             print(ret.shape)
             model, inliers = ransac((ret[:, 0], ret[:, 1]),
-                                    FundamentalMatrixTransform, min_samples=8,
+                                    #EssentialMatrixTransform,
+                                    FundamentalMatrixTransform,
+                                    min_samples=8,
                                     residual_threshold=0.01, max_trials=100)
 
             ret = ret[inliers]
+            s, v, d = np.linalg.svd(model.params)
+            print(v)
 
         self.last = {'kps': kps, 'des': des}
 
--- slam.py
+++ slam.py
@@ -5,10 +5,14 @@ from extractor import Extractor
 
 W = 1920//2
 H = 1080//2
+F = 1
+
+# Intrinsic Parameters Of The Camera
+K = np.array(([F, 0, W//2], [0, F, H//2], [0, 0, 1]))
 
 display = Display(W, H)
 
-fe = Extractor()
+fe = Extractor(K)
 
 
 def process_frame(img):
@@ -16,8 +20,8 @@ def process_frame(img):
     ret = fe.extract(img)
 
     for pt1, pt2 in ret:
-        u1, v1 = map(lambda x: int(round(x)), pt1)
-        u2, v2 = map(lambda x: int(round(x)), pt2)
+        u1, v1 = fe.denormalize(pt1)
+        u2, v2 = fe.denormalize(pt2)
 
         cv2.circle(img, (u1, v1), color=(0, 0, 255), radius=3)
         cv2.line(img, (u1, v1), (u2, v2), color=(0, 255, 0))
